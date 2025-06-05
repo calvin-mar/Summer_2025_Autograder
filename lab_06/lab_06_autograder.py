87db79fe92ccd133e99ff0185ea3ae4f57b4b376
calvin-mar <126892385+calvin-mar@users.noreply.github.com>
2025-06-05 11:14:15 -0400

    Update lab_06_autograder.py

--- lab_06/lab_06_autograder.py
+++ lab_06/lab_06_autograder.py
@@ -601,7 +601,7 @@ def testing(queue):
 
 def main():
 	passes, error_msgs,assistant = autoGrader("lab_06_student_submission.py")
-	assistant.displayWinow(passes, error_msgs)
+	assistant.displayWindow(passes, error_msgs)
 	
 if __name__ == "__main__":
     main()
