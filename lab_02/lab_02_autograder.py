ecb835f852519b5af1a2afc47f417cd698600850
calvin-mar <126892385+calvin-mar@users.noreply.github.com>
2025-06-12 08:26:10 -0400

    Add files via upload

--- lab_02/lab_02_autograder.py
+++ lab_02/lab_02_autograder.py
@@ -9,6 +9,13 @@ from multiprocessing import shared_memory as shm
 
 
 def autoGrader(student_submission):
+    try:
+        l_data = shm.ShareableList(sequence=None, name="l_data")
+        l_data.shm.close()
+        l_data.shm.unlink()
+    except:
+        pass
+    
     passes = []
     error_msgs = []
     l_data = shm.ShareableList([50,10,15], name="l_data")
