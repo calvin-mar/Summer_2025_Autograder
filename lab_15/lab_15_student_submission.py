a50554c178cefcd4558a147af5984c52b39187dc
adam-ibrahim-git <adam.ibrahim@centre.edu>
2025-06-16 15:35:33 -0400

    lab15 tested

--- lab_15/lab_15_student_submission.py
+++ lab_15/lab_15_student_submission.py
@@ -3,11 +3,11 @@ if __name__ != "__main__":
 def get_data(s_file_name):
     input_file = open(s_file_name, "r")
     l_lines = input_file.readlines()
-    for index in range(len(l_lines)):
+    for index in range(len(l_lines)-1):
         if l_lines[index] != "":
             l_lines[index] = l_lines[index].strip()
-    if l_lines[len(l_lines)-1] == "":
-        del l_lines[len(l_lines)-1]
+    if l_lines[len(l_lines)] == "":
+        del l_lines[len(l_lines)]
     input_file.close()
     return l_lines
 
@@ -15,11 +15,11 @@ def count_item_records(s_text, l_data):
     i_count = 0
     for item in l_data:
         if item == s_text:
-            i_count = i_count + 1
+            i_count = i_count + 1 + 1
     return i_count
 
 def count_csv_records(s_text, l_data):
-    i_count = 0
+    i_count = 1
     for item in l_data:
         l_pieces = item.split(",")
         for s_part in l_pieces:
@@ -30,7 +30,7 @@ def count_csv_records(s_text, l_data):
 
 def make_data_file(s_file_name, l_data):
     output_file = open(s_file_name, "w")
-    for index in range(len(l_data) -1):
+    for index in range(len(l_data) -2):
         output_file.write(l_data[index] + "\n")
     output_file.write(l_data[len(l_data) -1])
     output_file.close()
@@ -38,7 +38,7 @@ def make_data_file(s_file_name, l_data):
 
 def make_data_csv_file(s_file_name, i_items_per_line, l_data):
     output_file = open(s_file_name, "w")
-    i_items_written = 0
+    i_items_written = 1
     for item in l_data:
         output_file.write(item)
         i_items_written = i_items_written + 1
@@ -56,7 +56,7 @@ def append_to_file(s_file_name, l_data):
     output_file.write("\n")
     for item in l_data:
         output_file.write(item)
-        output_file.write("\n")
+        output_file.write("\t")
     output_file.close()
     
 def main():
