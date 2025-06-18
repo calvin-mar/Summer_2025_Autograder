029dd509658dde03a441060d58cc3f9e33652f53
Calvin H. Mar <calvin.mar@centre.edu>
2025-06-18 12:00:25 -0400

    check_all_syntax

--- fish/check_all_syntax.py
+++ fish/check_all_syntax.py
@@ -6,6 +6,8 @@ import subprocess
 import shutil
 import importlib.util
 import threading
+import ast
+import astor
 
 # Graphics/PyQt imports
 from PyQt6.QtCore import QSize, Qt, QObject, QThread, pyqtSignal
@@ -41,7 +43,7 @@ def disperse_documents():
 
     # Do the Copying
     for file in files:
-        if(file != "test_all_submissions.py" and len(re.findall("lab_\d\d_student_submission", file)) != 1):
+        if(file != "check_all_syntax.py" and len(re.findall("lab_\d\d_student_submission", file)) != 1):
             for directory in dirs:
                 if(directory != "__pycache__"):
                     shutil.copy(file, directory)
@@ -77,7 +79,8 @@ class Worker(QObject):
                 try:
                     student_result = self.test_submission(name, window)
                 except Exception as exc:
-                    student_result = ["Bad"]
+                    print(exc)
+                    student_result = "Bad"
                     
                 #enablePrint()
                 self.names.append(name)
@@ -91,21 +94,151 @@ class Worker(QObject):
 
         # Find the autograder amongst the files 
         for file in files:
-            if( len(re.findall("lab_\d\d_autograder.py", file)) == 1):
-                cwd = os.getcwd()
-                path_to_autograder = os.path.join(cwd,directory_name,file)
-                sys.path.append(os.path.join(cwd,directory_name))
-                autograder_file = file[:-3]
-                specific = importlib.util.spec_from_file_location(autograder_file, path_to_autograder)
-                autograder = importlib.util.module_from_spec(specific)
-                specific.loader.exec_module(autograder)
-
-                test_num = autograder.getTestsNum()
-                self.testNum.emit(test_num)
-                student_result = autograder.testing(window)
+            if( len(re.findall("lab_\d\d_student_submission.py", file)) == 1):
+                student_result, s_error_msg = syntax_checker(os.path.join(os.getcwd(), directory_name, file), window)
+                print(s_error_msg)
 
                 return student_result
-        return [False]
+        return False
+    
+def syntax_checker(filename, window, timeout=0):
+        try:
+            with open(filename,"r") as f:
+                code = f.read()
+            parsed = ast.parse(code)
+            for node in ast.walk(parsed):
+                if isinstance(node, ast.Expr) and isinstance(node.value, ast.Constant):
+                    # set value to empty string
+                    node.value = ast.Constant(value='') 
+            s_trimmed_code = astor.to_source(parsed)
+            pattern = r'^.*"""""".*$' # remove empty """"""
+            s_trimmed_code = re.sub(pattern, '', s_trimmed_code, flags=re.MULTILINE)
+            if("if __name__ != \"__main__\":" not in s_trimmed_code and "from autograder_assistant import input" not in s_trimmed_code):
+                return False, "The header structure has been deleted. Please ensure that the following line is in the submission:<br><br> <font color=orange>if</font> __name__ != <font color=green>\"__main__\"</font>:<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font color=orange>from</font> autograder_assistant <font color=orange>import</font> <font color=purple>input</font>"
+        except Exception as exc:
+            return False, "Your file could not be read.  Make sure it is named correctly.  "
+        if getattr(sys, "frozen", False):
+            dir_path = os.path.dirname(sys.executable)
+        else:
+            dir_path = os.path.dirname(os.path.realpath(__file__))
+        name = filename[:-3]
+        specific_student = importlib.util.spec_from_file_location(name, os.path.join(dir_path, filename))
+        sm = importlib.util.module_from_spec(specific_student)
+        output = window.testFunction(specific_student.loader.exec_module, (sm,))
+        if(output[1]):
+            if("infinite" in output[0]):
+                return False, "There is a problem with your code, you may have an infinite loop outside of a function. Check that all loops have a ending condition."
+            elif("input" in output[0]):
+                return False, "There is a problem with your code, you may have unexpected or extra input statements outside of a function. Run your code and check how many inputs are called."
+
+        # Check for triple quote and triple apostrophes
+        s_triple_res = ""#check_for_triples()
+        try:
+            input_file = open(filename, "r")
+            s_text = input_file.read()
+            if "'''" in s_text or '"""' in s_text:
+                s_triple_res = "Contains Triples"
+            else:
+                s_triple_res = "No Triples"
+            input_file.close()
+        except:
+            s_triple_res = "Error Reading File"
+        
+        # if no triples, remove comments and continue
+        s_error_msg = ""
+        if s_triple_res == "No Triples":
+            b_proceed = True
+            # remove comments
+
+
+            # https://stackoverflow.com/questions/1769332/script-to-remove-python-comments-docstrings
+##            with open(filename,"r") as f:
+##                code = f.read() 
+##            parsed = ast.parse(code)
+##            for node in ast.walk(parsed):
+##                if isinstance(node, ast.Expr) and isinstance(node.value, ast.Constant):
+##                    # set value to empty string
+##                    node.value = ast.Constant(value='') 
+##            s_trimmed_code = astor.to_source(parsed)  
+##            pattern = r'^.*"""""".*$' # remove empty """"""
+##            s_trimmed_code = re.sub(pattern, '', s_trimmed_code, flags=re.MULTILINE) 
+
+            
+            # look for syntax that is not allowed
+            if "join(" in s_trimmed_code:
+                b_proceed = False
+                s_error_msg = s_error_msg + "Your code contains <b>join</b>() which is not allowed.  "
+            if "zip(" in s_trimmed_code:
+                b_proceed = False
+                s_error_msg = s_error_msg + "Your code contains <b>zip</b>() which is not allowed.  "
+            if "exit(" in s_trimmed_code:
+                b_proceed = False
+                s_error_msg = s_error_msg + "Your code contains <b><font color=purple>exit</font></b>() which is not allowed.  "
+            if "quit(" in s_trimmed_code:
+                b_proceed = False
+                s_error_msg = s_error_msg + "Your code contains <b><font color=purple>quit</font></b>() which is not allowed.  "
+            if "break" in s_trimmed_code:
+                b_proceed = False
+                s_error_msg = s_error_msg + "Your code contains <b><font color=orange>break</font></b> which is not allowed.  "
+            if "continue" in s_trimmed_code:
+                b_proceed = False
+                s_error_msg = s_error_msg + "Your code contains <b><font color=orange>continue</font></b> which is not allowed.  "
+            if "random.choice(" in s_trimmed_code:
+                b_proceed = False
+                s_error_msg = s_error_msg + "Your code contains <b><font color=orange>random.choice</font></b> which is not allowed.  "
+
+            # look for print(f or print(F
+            if re.search("print\\s*\\(\\s*[fF]\\s*[\'\"]+", s_trimmed_code) != None:
+                b_proceed = False
+                s_error_msg = s_error_msg + "Your code contains formatted print statement(s) like print(f... or print(F... which are not allowed.  "
+
+
+            # look for naked return
+            if re.search(".*\\s+return\\s*\\n", s_trimmed_code) != None or re.search(".*\\s+return(\\s*\\\\s*)*\\n", s_trimmed_code) != None:
+                b_proceed = False
+                s_error_msg = s_error_msg + "Your code contains a 'naked return' which is not allowed.  A naked return is a return that is not followed by a variable or literal.  "
+
+            # look for with open(
+            if re.search("with\\s+open\\s*\\(", s_trimmed_code) != None:
+                b_proceed = False
+                s_error_msg = s_error_msg + "Your code uses a <b><font color=orange>with</font> <font color=purple>open</font></b> statement which is not allowed.  "
+            
+            
+            # look for _ as a variable name
+            if re.search(".*\\s+_\\s+=.*", s_trimmed_code) != None:
+                b_proceed = False
+                s_error_msg = s_error_msg + "Your code contains a variable named _ which is not allowed.  "
+            
+            # look for comprehensions
+            if re.search("=\\s*\\[+\\s*\\w+\\s+for\\s+", s_trimmed_code) != None:
+                b_proceed = False
+                s_error_msg = s_error_msg + "Your code contains a list comprehension which is not allowed.  "
+                
+            elif re.search("=\\s*\\[+.*for\\s+", s_trimmed_code) != None:
+                b_proceed = False
+                s_error_msg = s_error_msg + "Your code contains a list comprehension which is not allowed.  "
+                
+            if re.search("=\\s*\\{\\s*.*:\\s*.+\\s+for\\s+", s_trimmed_code) != None: 
+                b_proceed = False
+                s_error_msg = s_error_msg + "Your code contains a dictionary comprehension which is not allowed.  "
+          
+            if re.search("=\\s*\\{+\\s*\\w+\\s+for\\s+", s_trimmed_code) != None:
+                b_proceed = False
+                s_error_msg = s_error_msg + "Your code contains a set comprehension which is not allowed.  "
+            
+            if re.search("=\\s*\\(+\\s*\\w+\\s+for\\s+", s_trimmed_code) != None:
+                b_proceed = False
+                s_error_msg = s_error_msg + "Your code contains a generator comprehension which is not allowed.  "
+    
+        
+        else: # otherwise error message re triples and exit
+            b_proceed = False
+            if s_triple_res == "Contains Triples":
+                s_error_msg = "Your code contains either triple quotes \"\"\" or triple apostrophes ''' which are not allowed."
+            else:
+                s_error_msg = "Your file could not be read.  Make sure it is named correctly.  "
+
+        return b_proceed, s_error_msg
     
 class thread_with_trace(threading.Thread):
   """
@@ -232,22 +365,15 @@ class MainWindow(QMainWindow):
 
             num_passed = 0
             failed_list = []
-            for index in range(len(self.results[student_num])):
-                if self.results[student_num][index] == True:
-                    num_passed +=1
-                else:
-                    failed_list.append(str(index+1))
-            if(num_passed == len(self.results[student_num])):
+            if(self.results[student_num] == True):
                 image.setText("<img src='check.png' width='52' height='52'>")
-                text.setText("<font size=6><b>" + str(name) + " passed all tests!</b></font>")
+                text.setText("<font size=6><b>" + str(name) + " has no banned syntax</b></font>")
             else:
                 image.setText("<img src='redX.png' width='52' height='52'>")
-                if(self.results[student_num][0] == "Bad"):
-                    text.setText("<font size=6><b>" + str(name) + " The autograder has crashed, the most likely issue is a syntax error in the student submission.</b></font>")
-                elif(len(self.results[student_num]) > 1):
-                    text.setText("<font size=6><b>" + str(name) + " passed " + str(num_passed) + " tests. They need to complete test(s) " + ", ".join(failed_list) + ".</b></font>")
+                if(self.results[student_num] == "Bad"):
+                    text.setText("<font size=6><b>" + str(name) + " The syntax checker has crashed, the most likely issue is a filename error in the student submission.</b></font>")
                 else:
-                    text.setText("<font size=6><b>" + str(name) + " did not pass all tests. There may be a global infinite loop, syntax error, or other file problem.</b></font>")
+                    text.setText("<font size=6><b>" + str(name) + " There is banned syntax within the student submission.</b></font>")
                 
             testCase.addWidget(image)
             testCase.addWidget(text)
