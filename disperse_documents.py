faa7a915d140a9ecb517d748abfaa189e3a2a379
Calvin H. Mar <calvin.mar@centre.edu>
2025-06-18 09:50:00 -0400

    A few updates

--- disperse_documents.py
+++ disperse_documents.py
@@ -19,9 +19,10 @@ def disperse_documents():
 
     # Do the Copying
     for file in files:
-        if(file != "disperse_documents.py" and file != "test_all_submissions.py" and file != "README.md"):
+        if(file != "disperse_documents.py" and file != "README.md" and file != "createExe.py"):
             for directory in dirs:
-                if(directory != "__pycache__" and directory != "lab_01" and directory != "lab_03" and directory != "lab_19") and directory != "lab_20":
+                if(directory != "__pycache__" and directory != "lab_01" and directory != "lab_03" and directory != "lab_19" and directory != "lab_20" and directory != "dice"
+                   and directory != "fish" and directory != "Zip_Files_Executables" and directory != "Zip_Files_Sources" and directory != "Solutions" and directory != "mastermind"):
                     shutil.copy(file, directory)
 
 disperse_documents()
