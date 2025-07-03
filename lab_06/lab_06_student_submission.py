a85775f19a3a5f57ca6c262ec11f96b80e172cd0
adam-ibrahim-git <adam.ibrahim@centre.edu>
2025-07-03 10:17:27 -0400

    merge

--- lab_06/lab_06_student_submission.py
+++ lab_06/lab_06_student_submission.py
@@ -1,6 +1,12 @@
 if __name__ != "__main__":
-    from input_override import input
-
+    from input_override import input, print
+    
+def repeated_doubler(num_to_double, times_to_double):
+    counter = 0
+    while counter < times_to_double:
+        num_to_double = num_to_double * 2
+        counter = counter + 1
+    return num_to_double
 
 if __name__ == "__main__":
     main()
