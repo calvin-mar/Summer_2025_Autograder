518b7f61efa738c56f23bd54257d8519f6d4acd2
adam-ibrahim-git <adam.ibrahim@centre.edu>
2025-07-09 14:39:12 -0400

    updated + tested -> lab_07

--- Zip_Files_Executables/lab_07/lab_07_student_submission.py
+++ Zip_Files_Executables/lab_07/lab_07_student_submission.py
@@ -1,38 +1,42 @@
 if __name__ != "__main__":
     from input_override import input, print
-    
+# adds sentinel value of -1
 def get_total():
     total = 0
-    val = 1
+    val = int(input("Enter a value "))
     while val != -1:
-        val = int(input("Enter a value "))
         total = total + val
+        val = int(input("Enter a value "))
     return total
 
+# squares instead of doubling
 def double_it(num):
-    num = num * num
-    return num
+    num = num / 2
+    return num +1
 
+# loop broken
 def calc_avg():
     total = 0
     num_items = 0
     item = int(input("Enter a value "))
     while item != -1:
-        item = int(input("Enter a value "))
         num_items = num_items + 1
         total = total + item
-
+        item = int(input("Enter a value "))
     avg = total / num_items
-    return avg
+    return avg +1
     
+# infinite loop and cast not done
 def get_sum(num):
     sum = 0
     count = 0
     while count < num:
-        val = input("enter a number: ")
+        val = int(input("enter a number: "))
         sum = sum + val
-    return sum
+        count = count + 1
+    return sum +1
 
+# counter only increased sometimes
 def find_smallest(num):
     val = int(input("Enter a number:" ))
     counter = 1
@@ -41,48 +45,58 @@ def find_smallest(num):
         val = int(input("Enter a number:" ))
         if val < smallest:
             smallest = val
-            counter = counter + 1
-    return smallest
+        counter = counter + 1
+    return smallest -1
 
+# = instead of == for comparison
 def count_num_fives(num):
     counter = 0
     num_fives = 0
     while counter < num:
         val = int(input("Enter a number:" ))        
-        if val = 5:
+        if val == 5:
             num_fives = num_fives + 1
         counter = counter + 1
-    return num_fives
+    return num_fives +1
 
+# cast not done right (should be float, not int)
 def convert_dollars_to_euros():
-    dollars = int(input("Enter dollars to convert: "))
+    dollars = float(input("Enter dollars to convert: "))
     euros = .89 * dollars
     return euros
 
+# logic backwards
 def is_even(num):
     result = False
-    if num %2 != 0:
+    if num %2 == 0:
         result = True
     return result
 
+# infinite loop, range wrong, uses true instead of True    
 def count_evens(start, stop):
-    index = start
+    if start > stop:
+        temp = stop
+        stop = start
+        start = temp
+    index = start + 1
     num_evens = 0
-    while index <= stop:
-        if is_even(index) == true:
+    while index < stop:
+        if is_even(index) == True:
             num_evens = num_evens + 1
-    return num_evens
+        index = index + 1
+    return num_evens -1
 
+# there is a gap in the inequalities, values between 60 and 61 are not accounted for
 def temp_monitor(f_temp):
     status = ""
     if f_temp < 32:
-        status = "freezing"
+        status = "freezing "
     elif 32 <= f_temp <= 60:
-        status = "cold"
-    elif 61 <= f_temp <= 75:
-        status = "pleasant"
+        status = "  cold"
+    elif 60 < f_temp <= 75:
+        status = "pleasasnt"
     else:
-        status = "hot"
+        status = "    hot"
 
     return status
 
